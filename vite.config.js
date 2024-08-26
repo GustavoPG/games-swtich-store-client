41b4b7e47c898ad9d1574683f7e9e1da47990ee9
GustavoPG <gustavoparadag66@gmail.com>
2024-08-25 22:40:41 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',
+    port: 3000, // o el puerto que prefieras
+  },
+});
